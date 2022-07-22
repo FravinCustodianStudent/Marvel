c53476cca085629d8fd745c9573012f9ca6b4b7e
FravinCustodianStudent <91590998+FravinCustodianStudent@users.noreply.github.com>
2022-07-22 11:30:27 +0300

    PreRealse update

--- src/services/MarvelServices.js
+++ src/services/MarvelServices.js
@@ -1,8 +1,10 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
+    
+
     _apiKey = 'apikey=77f3c1231b69e74e4dc1762e4833fe1e';
 
+    _baseOffset = 210;
     getResource = async (url) => {
         let res = await fetch(url);
     
@@ -13,12 +15,26 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+    _transformCharacter=(char)=>{
+        
+        return{
+            id:char.id,
+            name:char.name,
+                description: char.description,
+                thumbnail:char.thumbnail.path + '.'+ char.thumbnail.extension,
+                homepage:char.urls[0].url,
+                wiki:char.urls[1].url,
+                comics: char.comics.items
+        }
     }
 }
 
